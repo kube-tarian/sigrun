4213fa0ddf0e03e2ac5e010e2353b1ad7ccc1b37
Shravan Shetty <shravanshetty322@gmail.com>
2021-09-13 22:37:10 +0530

    sigrun controller now has limited permissions

--- scripts/gen-crt.sh
+++ scripts/gen-crt.sh
@@ -1,27 +1,5 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2019 StackRox Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# generate-keys.sh
-#
-# Generate a (self-signed) CA certificate and a certificate and private key to be used by the webhook demo server.
-# The certificate will be issued for the Common Name (CN) of `webhook-server.webhook-demo.svc`, which is the
-# cluster-internal DNS name for the service.
-#
-# NOTE: THIS SCRIPT EXISTS FOR DEMO PURPOSES ONLY. DO NOT USE IT FOR YOUR PRODUCTION WORKLOADS.
-
 
 key_dir="./tmp"
 mkdir "$key_dir"
