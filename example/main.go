0868d553703f2931d84b4e4ac1385495e189f9a0
Shravan Shetty <shravanshetty322@gmail.com>
2022-01-23 01:22:35 +0530

    fixing image name

--- example/main.go
+++ example/main.go
@@ -9,7 +9,7 @@ func main() {
 	log.Println("Starting server on port 9000.....")
 	log.Fatal(http.ListenAndServe(":9000", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
-		_, err := w.Write([]byte("Hello world12"))
+		_, err := w.Write([]byte("Hello world1"))
 		if err != nil {
 			log.Println(err)
 			return
